75389647754809822c308db459a01bfd25d45f42
CelineXuereb <Celinecx02@gmail.com>
2023-07-28 00:15:43 +0200

    Updating contact form and insert cards
    Updated contact form
    insert album cards, and two container strips in about page

---
+++ Html, Css ,Images/Javascript/contactform.js
@@ -0,0 +1,7 @@
+cons inputs = document.querySelectorAll(".contact-input");
+
+inputs.forEach(ipt => {
+    ipt.addEventListener("focus", ()=>{
+        ipt.parentNode.classList.add("focus");
+    })
+})
