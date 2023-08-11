cd01184bd47387730153e87d9e3218736e4b8274
CelineXuereb <Celinecx02@gmail.com>
2023-08-11 23:47:24 +0200

    Adjustments applied
    Adjusted Image of "International TourDates" in Home Page
    
    Applied Footer
    
    Switched Poster to the right side of container "taylor swift white and blue side view" poster, to leave more room for Increase and Decrease font side tool.

--- html,css,images/javascript/contactform.js
+++ html,css,images/javascript/contactform.js
@@ -1,7 +1,10 @@
-cons inputs = document.querySelectorAll(".contact-input");
+const inputs = document.querySelectorAll(".contact-input");
 
-inputs.forEach(ipt => {
+inputs.forEach((ipt) => {
     ipt.addEventListener("focus", ()=>{
         ipt.parentNode.classList.add("focus");
-    })
-})
+    });
+    ipt.addEventListener("blur", ()=> {
+        iptparentNode.classList.remove("focus");
+    });
+});
